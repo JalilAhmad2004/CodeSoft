110e4a11ba007a63ab192abd872411e99bb5fcb5
M. Jalil Ahmad <117964245+JalilAhmad2004@users.noreply.github.com>
2023-10-21 13:22:39 +0500

    Update CodSoft_Task2.cpp

--- CodSoft_Task2.cpp
+++ CodSoft_Task2.cpp
@@ -1,69 +1,71 @@
 #include <iostream>
-#include <cstring>
 using namespace std;
 
 int main()
 {
-    //Calculator functionality
+    // Calculator functionality
     double num1;
     double num2;
+    double result;
     char op;
-    bool choice;
+    int choice = 1; // Initialize choice to 1
 
-    cout << "Enter 1st number:";
+    cout << "Enter 1st number: ";
     cin >> num1;
 
-    cout << "Enter 2nd number:";
-    cin >> num2;
+    result = num1;
 
-    cout << "Enter Operator:";
-    cin >> op;
-
-    // Conditions to check the operator
-    if (op == '+')
-        cout << "Result:" << num1 + num2;
-    else if (op == '-')
-        cout << "Result:" << num1 - num2;
-    else if (op == '*')
-        cout << "Result:" << num1 * num2;
-    else if (op == '/')
-        cout << "Result:" << num1 / num2;
-    else
-        cout << "Invalid Operator";
-
-    cout << endl;
-
-    // Loop to perform any other operation
     do
     {
-        cout << "\nEnter 1 to perform any other operation and 0 to Quit:";
-        cin >> choice;
-        switch (choice)
+        cout << "Enter Operator (+, -, *, /) or any other key to quit: ";
+        cin >> op;
+
+        if (op == '+' || op == '-' || op == '*' || op == '/')
         {
-        case 0:
-            cout << "Thanks for using my calculator\n";
-        case 1:
-            cout << "Enter 2nd number:";
+            cout << "Enter 2nd number: ";
             cin >> num2;
 
-            cout << "Enter Operator:";
-            cin >> op;
-
             // Conditions to check the operator
-            if (op == '+')
-                cout << "Result:" << num1 + num2;
-            else if (op == '-')
-                cout << "Result:" << num1 - num2;
-            else if (op == '*')
-                cout << "Result:" << num1 * num2;
-            else if (op == '/')
-                cout << "Result:" << num1 / num2;
-            else
-                cout << "Invalid Operator";
+            switch (op)
+            {
+            case '+':
+                result += num2;
+                cout << "Result: " << result << endl;
+                break;
+            case '-':
+                result -= num2;
+                cout << "Result: " << result << endl;
+                break;
+            case '*':
+                result *= num2;
+                cout << "Result: " << result << endl;
+                break;
+            case '/':
+                if (num2 == 0)
+                {
+                    cout << "Error: Division by zero" << endl;
+                }
+                else
+                {
+                    result /= num2;
+                    cout << "Result: " << result << endl;
+                }
+                break;
+            }
         }
-    } while (choice);
+        else
+        {
+            cout << "Invalid Operator" << endl;
+            break; // Break the loop if an invalid operator is entered
+        }
 
-    cout << endl;
+        cout << "\nEnter 1 to perform another operation or any other number to Quit: ";
+        cin >> choice;
+
+    } while (choice == 1);
+
+    cout << "\nThanks for using my calculator" << endl;
+
     return 0;
 }
 
